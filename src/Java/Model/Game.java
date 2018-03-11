3a53ccbbdf51325d9aa9c8857a10411c16b42708
LMalko <lmalko2601@gmail.com>
2018-03-11 23:35:31 +0100

    Add serialVersionUID field in Game.java

--- src/Java/Model/Game.java
+++ src/Java/Model/Game.java
@@ -4,6 +4,8 @@ import java.awt.Canvas;
 
 public class Game extends Canvas implements Runnable{
 
+    private static final long serialVersionUID = 1L;
+
     public void startApp(){
         Window window = new Window(1000, 563, "Wizard game", this);
     }
