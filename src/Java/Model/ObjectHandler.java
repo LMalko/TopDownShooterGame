dfcc962fed2d8cb108f8af60276c7457a2a38263
LMalko <lmalko2601@gmail.com>
2018-03-12 00:54:56 +0100

    Add getters and setters to ObjectHandler.java

--- src/Java/Model/ObjectHandler.java
+++ src/Java/Model/ObjectHandler.java
@@ -36,4 +36,44 @@ public class ObjectHandler {
         public void removeGameObject(GameObject gameObject){
                 objectsCollection.remove(gameObject);
         }
+
+        public List<GameObject> getObjectsCollection() {
+                return objectsCollection;
+        }
+
+        public void setObjectsCollection(List<GameObject> objectsCollection) {
+                this.objectsCollection = objectsCollection;
+        }
+
+        public boolean isUp() {
+                return up;
+        }
+
+        public void setUp(boolean up) {
+                this.up = up;
+        }
+
+        public boolean isDown() {
+                return down;
+        }
+
+        public void setDown(boolean down) {
+                this.down = down;
+        }
+
+        public boolean isRight() {
+                return right;
+        }
+
+        public void setRight(boolean right) {
+                this.right = right;
+        }
+
+        public boolean isLeft() {
+                return left;
+        }
+
+        public void setLeft(boolean left) {
+                this.left = left;
+        }
 }
