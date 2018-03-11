e42ad07f15e0812dc739244346357ad185853846
LMalko <lmalko2601@gmail.com>
2018-03-12 00:11:32 +0100

    Create getters & setters in GameObject.java

--- src/Java/Abstract/GameObject.java
+++ src/Java/Abstract/GameObject.java
@@ -17,4 +17,36 @@ public abstract class GameObject {
         public abstract void tick();
         public abstract void render(Graphics graphics);
         public abstract Rectangle getBounds();
+
+        public int getX() {
+                return x;
+        }
+
+        public void setX(int x) {
+                this.x = x;
+        }
+
+        public int getY() {
+                return y;
+        }
+
+        public void setY(int y) {
+                this.y = y;
+        }
+
+        public float getVelocityX() {
+                return velocityX;
+        }
+
+        public void setVelocityX(float velocityX) {
+                this.velocityX = velocityX;
+        }
+
+        public float getVelocityY() {
+                return velocityY;
+        }
+
+        public void setVelocityY(float velocityY) {
+                this.velocityY = velocityY;
+        }
 }
