915e4d089b30644003aa62e18b5b3b4c8f9408f3
LMalko <lmalko2601@gmail.com>
2018-03-12 00:16:41 +0100

    Add objects list field in ObjectHandler.java

--- src/Java/Model/ObjectHandler.java
+++ src/Java/Model/ObjectHandler.java
@@ -1,4 +1,11 @@
 package Java.Model;
 
+import Java.Abstract.GameObject;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class ObjectHandler {
+
+        List<GameObject> objectsCollection = new LinkedList<>();
 }
