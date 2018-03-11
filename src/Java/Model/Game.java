eb213146e3dd568e2777eed08d6363cc6267c257
LMalko <lmalko2601@gmail.com>
2018-03-11 23:26:44 +0100

    Extend Game.java with Canvas and implement Runnable

--- src/Java/Model/Game.java
+++ src/Java/Model/Game.java
@@ -1,4 +1,15 @@
 package Java.Model;
 
-public class Game {
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable{
+
+    public void startApp(){
+//will create Window object;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
