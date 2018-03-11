10b0b7ecdb8840dcbcb90d560f0c1f6f623f502e
LMalko <lmalko2601@gmail.com>
2018-03-12 00:45:54 +0100

    Create class Warlock.java

---
+++ src/Java/Model/Warlock.java
@@ -0,0 +1,13 @@
+package Java.Model;
+
+import Java.Abstract.GameObject;
+import Java.Enums.ID;
+
+public class Warlock extends GameObject{
+
+        public Warlock(int x, int y, ID id) {
+                super(x, y, id);
+        }
+
+        
+}
