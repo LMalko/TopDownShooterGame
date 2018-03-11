aad93223c089b785eca97e658af9696bb52c2046
LMalko <lmalko2601@gmail.com>
2018-03-12 00:51:13 +0100

    Create KeyInput.java

---
+++ src/Java/Model/KeyInput.java
@@ -0,0 +1,15 @@
+package Java.Model;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyInput extends KeyAdapter{
+
+        public void keyPressed(KeyEvent keyEvent){
+
+        }
+        public void keyReleased(KeyEvent keyEvent){
+
+        }
+
+}
