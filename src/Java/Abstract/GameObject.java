d67174ad51cd3392ebde1884cc10cf9bdd92a6cd
LMalko <lmalko2601@gmail.com>
2018-03-12 00:09:21 +0100

    Declare fields in GameObject.java

--- src/Java/Abstract/GameObject.java
+++ src/Java/Abstract/GameObject.java
@@ -2,5 +2,9 @@ package Java.Abstract;
 
 public abstract class GameObject {
 
+        protected int x;
+        protected int y;
+        protected float velocityX = 0;
+        protected float velocityY = 0;
 
 }
