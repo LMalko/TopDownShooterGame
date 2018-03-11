2b817a83159bb06336df40dbc4d9847cfd320ea1
LMalko <lmalko2601@gmail.com>
2018-03-12 00:26:07 +0100

    Add tick() and ObjectHandler object in Game.java

--- src/Java/Model/Game.java
+++ src/Java/Model/Game.java
@@ -9,44 +9,51 @@ public class Game extends Canvas implements Runnable{
 
     private boolean isRunning = false;
     private Thread thread;
+    private ObjectHandler objectHandler;
 
     public void startApp(){
         Window window = new Window(1000, 563, "Wizard game", this);
+        objectHandler = new ObjectHandler();
     }
 
     @Override
     public void run() {
-            this.requestFocus();
-            long lastTime = System.nanoTime();
-            double amountOfTicks = 60.0;
-            double ns = 1000000000 / amountOfTicks;
-            double delta = 0;
-            long timer = System.currentTimeMillis();
-            int frames = 0;
+        this.requestFocus();
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
 
-            while(isRunning) {
-                long now = System.nanoTime();
-                delta += (now - lastTime) / ns;
-                lastTime = now;
-                while(delta >= 1) {
-                    tick();
-                    //updates++;
-                    delta--;
-                }
-                render();
-                frames++;
-
-                if(System.currentTimeMillis() - timer > 1000) {
-                    timer += 1000;
-                    frames = 0;
-                    //updates = 0;
-                }
+        while (isRunning) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                tick();
+                //updates++;
+                delta--;
             }
-            stop();
-        }﻿
+            render();
+            frames++;
+
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                frames = 0;
+                //updates = 0;
+            }
+        }
+        stop();
+    }
+
+    public void tick(){
+        /* Update everything in the game. */
+        objectHandler.tick();
+    }
 
     public void render(){
-//        Render everything in the game
+//        Render everything in the game.
         BufferStrategy bufferStrategy = this.getBufferStrategy();
         if(bufferStrategy == null){
             this.createBufferStrategy(3);
@@ -54,6 +61,7 @@ public class Game extends Canvas implements Runnable{
         }
 
         Graphics graphics = bufferStrategy.getDrawGraphics();
+        objectHandler.render(graphics);
         graphics.dispose();
         bufferStrategy.show();
     }
