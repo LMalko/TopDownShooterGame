dbb3993d09e7a4701562108bb760b792bae800bb
LMalko <lmalko2601@gmail.com>
2018-03-11 23:15:52 +0100

    Add fields and constructor to Window

--- src/Java/Model/Window.java
+++ src/Java/Model/Window.java
@@ -1,4 +1,16 @@
 package Java.Model;
 
 public class Window {
+
+    private int width;
+    private int height;
+    private String title;
+    private Game game;
+
+    public Window(int width, int height, String title, Game game) {
+        this.width = width;
+        this.height = height;
+        this.title = title;
+        this.game = game;
+    }
 }
