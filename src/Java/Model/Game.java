8c680c6c588ebfe95f6e3911ea92a676d57992f1
LMalko <lmalko2601@gmail.com>
2018-03-11 23:37:56 +0100

    Add initial fields in Game.java

--- src/Java/Model/Game.java
+++ src/Java/Model/Game.java
@@ -6,6 +6,9 @@ public class Game extends Canvas implements Runnable{
 
     private static final long serialVersionUID = 1L;
 
+    private boolean isRunning = false;
+    private Thread thread;
+
     public void startApp(){
         Window window = new Window(1000, 563, "Wizard game", this);
     }
