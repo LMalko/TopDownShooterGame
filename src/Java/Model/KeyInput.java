2044fe4c7fe285ae5f9fa6c01cb0200690ac861d
LMalko <lmalko2601@gmail.com>
2018-03-12 00:56:11 +0100

    Add ObjectHandler instance and constructor in KeyInput.java

--- src/Java/Model/KeyInput.java
+++ src/Java/Model/KeyInput.java
@@ -5,10 +5,17 @@ import java.awt.event.KeyEvent;
 
 public class KeyInput extends KeyAdapter{
 
+        ObjectHandler handler;
+
+        public KeyInput(ObjectHandler handler) {
+                this.handler = handler;
+        }
+
         public void keyPressed(KeyEvent keyEvent){
 
         }
         public void keyReleased(KeyEvent keyEvent){
+
 
         }
 
