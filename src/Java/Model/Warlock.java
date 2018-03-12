bdeb41c23713226ff900eade2f429fc788463176
LMalko <lmalko2601@gmail.com>
2018-03-12 01:12:27 +0100

    Add ObjectHandler and fill tick() in Warlock.java

--- src/Java/Model/Warlock.java
+++ src/Java/Model/Warlock.java
@@ -3,15 +3,55 @@ package Java.Model;
 import Java.Abstract.GameObject;
 import Java.Enums.ID;
 
+import java.awt.*;
+
 public class Warlock extends GameObject{
 
-        public Warlock(int x, int y, ID id) {
+        ObjectHandler handler;
+
+        public Warlock(int x, int y, ID id, ObjectHandler handler) {
                 super(x, y, id);
+                this.handler = handler;
         }
 
         public void tick(){
                 x += velocityX;
                 y += velocityY;
+
+                if(handler.isUp()){
+                        velocityY -= 5;
+                }else if(!handler.isDown()){
+                        velocityY = 0;
+                }
+
+                if(handler.isDown()){
+                        velocityY -= 5;
+                }else if(!handler.isUp()){
+                        velocityY = 0;
+                }
+
+                if(handler.isLeft()){
+                        velocityY -= 5;
+                }else if(!handler.isRight()){
+                        velocityY = 0;
+                }
+
+                if(handler.isRight()){
+                        velocityY -= 5;
+                }else if(!handler.isLeft()){
+                        velocityY = 0;
+                }
+
+        }
+
+        @Override
+        public void render(Graphics graphics) {
+
+        }
+
+        @Override
+        public Rectangle getBounds() {
+                return null;
         }
 
 
