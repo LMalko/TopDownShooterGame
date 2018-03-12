986556b4ea1b0fe3c833a076efe0d699444acd23
LMalko <lmalko2601@gmail.com>
2018-03-12 16:03:46 +0100

    Add getBoundsBig & apply collicion in tick() in Enemy.java

--- src/Java/Model/Enemy.java
+++ src/Java/Model/Enemy.java
@@ -4,10 +4,14 @@ import Java.Abstract.GameObject;
 import Java.Enums.ID;
 
 import java.awt.*;
+import java.util.Random;
 
 public class Enemy extends GameObject {
 
         private ObjectHandler handler;
+        Random random = new Random();
+        int choose = 0;
+        int hp = 100;
 
         public Enemy(int x, int y, ID id, ObjectHandler handler) {
                 super(x, y, id);
@@ -18,6 +22,25 @@ public class Enemy extends GameObject {
         public void tick() {
                 x += velocityX;
                 y += velocityY;
+
+                choose = random.nextInt(10);
+
+                for(int i = 0; i < handler.objectsCollection.size(); i++){
+                        GameObject tempObject = handler.objectsCollection.get(i);
+                        if(tempObject.getId() == ID.Block){
+                                if(getBoundsBig().intersects(tempObject.getBounds())){
+                                        velocityX += (velocityX*2) * -1;
+                                        velocityY += (velocityY*2) * -1;
+
+
+                                }
+                        }
+                }
+
+                if(choose == 0){
+                        velocityX = (random.nextInt( 4 - -4) + -4);
+                        velocityY = (random.nextInt( 4 - -4) + -4);
+                }
         }
 
         @Override
@@ -30,5 +53,9 @@ public class Enemy extends GameObject {
         @Override
         public Rectangle getBounds() {
                 return new Rectangle(x, y, 32, 32);
+        }
+
+        public Rectangle getBoundsBig(){
+                return new Rectangle(x -16, y -16, 64, 64);
         }
 }
