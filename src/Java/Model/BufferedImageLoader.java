459ea220d1f3dd86ae6c8b0007019d93bd628775
LMalko <lmalko2601@gmail.com>
2018-03-12 11:16:51 +0100

    Add field and method to BufferedImageLoader.java

--- src/Java/Model/BufferedImageLoader.java
+++ src/Java/Model/BufferedImageLoader.java
@@ -1,4 +1,22 @@
 package Java.Model;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 public class BufferedImageLoader {
+
+        private BufferedImage image;
+
+        private BufferedImage loadImage(String path){
+
+                try {
+                        image = ImageIO.read(getClass().getResource(path));
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+
+        }
+        
+
 }
