2a9900fba580c34313b2c4cf27949e141e21d925
LMalko <lmalko2601@gmail.com>
2018-03-12 01:04:38 +0100

    Define keyPressed() in KeyInput.java

--- src/Java/Model/KeyInput.java
+++ src/Java/Model/KeyInput.java
@@ -1,5 +1,8 @@
 package Java.Model;
 
+import Java.Abstract.GameObject;
+import Java.Enums.ID;
+
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
@@ -12,6 +15,28 @@ public class KeyInput extends KeyAdapter{
         }
 
         public void keyPressed(KeyEvent keyEvent){
+
+                int key = keyEvent.getKeyCode();
+
+                for (int i = 0; i < handler.objectsCollection.size(); i++){
+                        GameObject tempObject = handler.objectsCollection.get(i);
+                        if (tempObject.getId() == ID.Player){
+                                switch (key){
+                                        case KeyEvent.VK_W:
+                                                handler.setUp(true);
+                                                break;
+                                        case KeyEvent.VK_S:
+                                                handler.setDown(true);
+                                                break;
+                                        case KeyEvent.VK_A:
+                                                handler.setLeft(true);
+                                                break;
+                                        case KeyEvent.VK_D:
+                                                handler.setRight(true);
+                                                break;
+                                }
+                        }
+                }
 
         }
         public void keyReleased(KeyEvent keyEvent){
